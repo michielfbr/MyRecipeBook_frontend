6b21a74ae66a4fb666f31484392f5787c02f120e
michielfbr <michiel_brongers@hotmail.com>
2021-11-26 14:33:24 +0100

    Ingredient fields added to component.RecipeForm and displaying & updating state

--- src/components/RecipeForm/index.js
+++ src/components/RecipeForm/index.js
@@ -7,35 +7,100 @@ import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
+import { selectUser } from "../../store/user/selectors";
 
 export default function RecipeForm({
   pageTitle,
   recipe,
   setRecipe,
-  submitForm,
+  submitRecipe,
 }) {
   const [title, setTitle] = useState(recipe.title);
   const [imageUrl, setImageUrl] = useState(recipe.imageUrl);
   const [cookingTime, setCookingTime] = useState(recipe.cookingTime);
+  const [ingredients, setIngredients] = useState([
+    { title: "test1", amount: 1, unit_singular: "l" },
+    { title: "test2" },
+  ]);
+  const [tags, setTags] = useState(recipe.tags);
   const [instructions, setInstructions] = useState(recipe.instructions);
   const [reference, setReference] = useState(recipe.reference);
+
+  const user = useSelector(selectUser);
+  const userId = user.id;
+
+  function submitForm(event) {
+    event.preventDefault();
+    console.log("Add recipe submitted");
+    setRecipe(
+      title,
+      imageUrl,
+      cookingTime,
+      ingredients,
+      tags,
+      instructions,
+      reference,
+      userId
+    );
+    submitRecipe();
+  }
+
+  // const [ingredientTitle, setIngredientTitle] = useState("");
+
+  function changeIngredientTitle(value, index) {
+    const newIngredients = [...ingredients];
+    newIngredients[index].title = value;
+    // console.log(newIngredients[index]);
+    setIngredients(newIngredients);
+    // console.log(ingredients[index]);
+    // console.log(ingredients);
+  }
+
+  function changeIngredientAmount(value, index) {
+    const newIngredients = [...ingredients];
+    newIngredients[index].amount = value;
+    // console.log(newIngredients[index]);
+    setIngredients(newIngredients);
+    // console.log(ingredients[index]);
+    // console.log(ingredients);
+  }
+
+  function changeIngredientUnitSingular(value, index) {
+    const newIngredients = [...ingredients];
+    newIngredients[index].unit_singular = value;
+    // console.log(newIngredients[index]);
+    setIngredients(newIngredients);
+    // console.log(ingredients[index]);
+    // console.log(ingredients);
+  }
+
+  function addIngredient() {
+    const newIngredients = [...ingredients, {}];
+    setIngredients(newIngredients);
+  }
+
+  function removeIngredient(index) {
+    console.log("delete");
+    const deletedIngredient = ingredients.splice(index, 1);
+    console.log(deletedIngredient);
+  }
 
   return (
     <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
       <h1 className="mt-5">{pageTitle}</h1>
 
-      <Form.Group controlId="formBasicEmail">
+      <Form.Group>
         <Form.Label>Recipe name</Form.Label>
         <Form.Control
           value={title}
           onChange={(event) => setTitle(event.target.value)}
-          type="text"
+          type="string"
           placeholder="Recipe title"
           required
         />
       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label>Image url</Form.Label>
         <Form.Control
           value={imageUrl}
@@ -46,7 +111,7 @@ export default function RecipeForm({
         />
       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label>Cooking Time</Form.Label>
         <Form.Control
           value={cookingTime}
@@ -55,8 +120,73 @@ export default function RecipeForm({
           required
         />
       </Form.Group>
+      <hr />
+      <div>
+        {ingredients.map((ing, index) => {
+          return (
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Row>
+                <Col xs={5}>
+                  <Form.Label>Ingredient {index}</Form.Label>{" "}
+                  {/* Delete {index} in production! */}
+                  <Form.Control
+                    value={ing.title}
+                    onChange={(event) =>
+                      changeIngredientTitle(event.target.value, index)
+                    }
+                    type="string"
+                    placeholder="Ingredient"
+                    required
+                  />
+                </Col>
+                <Col>
+                  <Form.Label>Amount</Form.Label>
+                  <Form.Control
+                    value={ing.amount}
+                    onChange={(event) =>
+                      changeIngredientAmount(event.target.value, index)
+                    }
+                    type="number"
+                    required
+                  />
+                </Col>
+                <Col>
+                  <Form.Label>Unit</Form.Label>
+                  <Form.Control
+                    value={ing.unit_singular}
+                    onChange={(event) =>
+                      changeIngredientUnitSingular(event.target.value, index)
+                    }
+                    as="select"
+                  >
+                    <option>g</option>
+                    <option>kg</option>
+                    <option>l</option>
+                    <option>ml</option>
+                    <option>teaspoon</option>
+                  </Form.Control>
+                </Col>
+                <Col>
+                  <Form.Label>Delete</Form.Label>
+                  <Button
+                    variant="secondary"
+                    onClick={() => removeIngredient(index)}
+                  >
+                    x
+                  </Button>
+                </Col>
+              </Row>
+            </Form.Group>
+          );
+        })}
+      </div>
 
-      <Form.Group controlId="ControlTextarea1">
+      {/* <Form.Label>Add ingredient</Form.Label> */}
+      <Button variant="primary" type="submit" onClick={addIngredient}>
+        +
+      </Button>
+      <hr />
+      <Form.Group>
         <Form.Label>Instructions</Form.Label>
         <Form.Control
           as="textarea"
@@ -69,7 +199,7 @@ export default function RecipeForm({
         />
       </Form.Group>
 
-      <Form.Group controlId="formBasicEmail">
+      <Form.Group>
         <Form.Label>Reference</Form.Label>
         <Form.Control
           value={reference}
@@ -79,10 +209,10 @@ export default function RecipeForm({
           required
         />
       </Form.Group>
-      
-  <Form.Group className="mb-3" id="formGridCheckbox">
+
+      {/* <Form.Group className="mb-3" id="formGridCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
+  </Form.Group> */}
 
       <Form.Group className="mt-5">
         <Button variant="primary" type="submit" onClick={submitForm}>
--- src/pages/AddRecipe/index.js
+++ src/pages/AddRecipe/index.js
@@ -4,15 +4,17 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { newRecipe } from "../../store/recipe/actions";
 
 export default function AddRecipe() {
   const pageTitle = "Add recipe";
   const [recipe, setRecipe] = useState("");
   const dispatch = useDispatch();
 
-  function submitForm(event) {
+  function submitRecipe(event) {
     event.preventDefault();
     console.log("Add recipe submitted");
+    dispatch(newRecipe(recipe));
   }
 
   return (
@@ -25,7 +27,7 @@ export default function AddRecipe() {
         pageTitle={pageTitle}
         recipe={recipe}
         setRecipe={setRecipe}
-        submitForm={submitForm}
+        submitForm={submitRecipe}
       />
     </Container>
   );
