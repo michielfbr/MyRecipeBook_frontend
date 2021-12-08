6c562da24ee14572c7219831ff5305119fb89abe
michielfbr <michiel_brongers@hotmail.com>
2021-12-08 19:26:16 +0100

    page.Signup form added | signup action changed to work

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+import {
+  Container,
+  Form,
+  Button,
+  Col,
+  Row,
+  FloatingLabel,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { signUp } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
-import { Col } from "react-bootstrap";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const navigate = useNavigate();
@@ -25,11 +32,12 @@ export default function SignUp() {
   function submitForm(event) {
     event.preventDefault();
 
-    dispatch(signUp(name, email, password));
+    dispatch(signUp(email, password, firstName, lastName));
 
-    setEmail("");
-    setPassword("");
-    setName("");
+    // setEmail("");
+    // setPassword("");
+    // setFirstName("");
+    // setLastName("");
   }
 
   return (
@@ -37,47 +45,77 @@ export default function SignUp() {
       <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
         <h2 className="Header">Welcome to MyRecipeBook</h2>
         <h4>A place to store and browse through the recipes you collect</h4>
-        {/* <Form.Group className="mt-5" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            type="text"
-            placeholder="Enter name"
-            required
-          />
+        <Form.Group className="mt-5" controlId="formBasicName">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="First name"
+            className="mb-3"
+          >
+            <Form.Control
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              type="text"
+              placeholder="Enter first name"
+              required
+            />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group controlId="formBasicName">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Last name"
+            className="mb-3"
+          >
+            <Form.Control
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              type="text"
+              placeholder="Enter last name"
+              required
+            />
+          </FloatingLabel>
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type="email"
-            placeholder="Enter email"
-            required
-          />
-          <Form.Text className="text-muted">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email"
+            className="mb-3"
+          >
+            <Form.Control
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </FloatingLabel>
+          {/* <Form.Text className="text-muted">
             We'll never share your email with anyone else.
-          </Form.Text>
+          </Form.Text> */}
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-            placeholder="Password"
-            required
-          />
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Password"
+            className="mb-3"
+          >
+            <Form.Control
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </FloatingLabel>
         </Form.Group>
-        <Form.Group className="mt-5">
+        {/* <Form.Group className="mt-5">
           <Button variant="primary" type="submit" onClick={submitForm}>
             Sign up
           </Button>
         </Form.Group> */}
         <Form.Group className="mt-5">
-          <Button variant="success" type="submit">
+          <Button variant="success" type="submit"onClick={submitForm}>
             Sign up
           </Button>{" "}
           <Link className="Link" to="/">
