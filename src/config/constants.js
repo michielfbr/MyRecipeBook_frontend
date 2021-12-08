c64d0ff4c903ab700cc20aace328a706ebb5cb40
michielfbr <michiel_brongers@hotmail.com>
2021-12-08 19:38:58 +0100

    API url updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 // export const apiUrl = process.env.API_URL || "http://localhost:4001";
 
-export const apiUrl = process.env.API_URL || "https://mfb-myrecipebook.herokuapp.com/";
+export const apiUrl = process.env.API_URL || "https://mfb-myrecipebook.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
